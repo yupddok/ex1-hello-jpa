5bd83a10b4a2b0d5667e60f4a47840731ad042f8
yupddok <yupddok@gmail.com>
2023-09-03 23:13:52 +0900

    Initial commit

---
+++ src/main/java/hello/jpa/JpaMain.java
@@ -0,0 +1,47 @@
+package hello.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        // persistenceUnitName
+        // /src/main/resources/META-INF/persistence.xml
+        // <persistence-unit name="hello">
+        // 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 엔티티 매니저는 쓰레드간에 공유X (사용하고 버려야 한다).
+        EntityManager em = emf.createEntityManager();
+
+        // JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 등록
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("helloC");
+            em.persist(member);
+
+            // 조회
+            Member findMember = em.find(Member.class, 3L);
+
+            // 수정
+            findMember.setName("helloC-수정");
+
+            // 삭제
+            em.remove(findMember);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
